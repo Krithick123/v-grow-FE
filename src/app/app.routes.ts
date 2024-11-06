65c18c2f23ad486196f5e4c6ae697884c66a42d6
Krithick123 <krithick423@gmail.com>
2024-11-06 22:02:29 +0530

    portfolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
 import { HomeComponent } from './components/home/home.component';
 import { ServicesComponent } from './components/services/services.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 export const routes: Routes = [
     {
@@ -16,5 +18,13 @@ export const routes: Routes = [
     {
         path:'services',
         component:ServicesComponent,
+    },
+    {
+        path:'about',
+        component:AboutComponent,
+    },
+    {
+        path:'portfolio',
+        component:PortfolioComponent,
     }
 ];
